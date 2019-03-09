ddac1c2c4b27152608ea981d9529b8205be726c2
Carley Tripp <carley.tripp@outlook.com>
2019-03-09 00:39:27 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,10 +1,2 @@
 module SongsHelper
-  
-def render_artist_or_edit_page
-  if @song.artist
-    <%= link_to @song.artist_name, artist_path(@song.artist) %>
-  else
-    <%= link_to "Add Artist", edit_song_path(@song) %>
-  end
-  
 end
